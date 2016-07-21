645f92ac8d174ee83af92512b07fe90a7bad48a1
Sohan Khadka <sohankhadka82@gmail.com>
2016-07-21 14:10:40 +0545

    late commit

---
+++ app/Models/Role.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sohan
+ * Date: 16/07/07
+ * Time: 1:13 PM
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+
+{
+    protected $fillable = ['name'];
+    
+
+}
---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles=[
+            "Admin",
+            "Member"
+        ];
+        foreach ($roles as $role ) {
+            \App\Models\Role::create([
+                'name'=>$role 
+            ]);
+        }
+
+    }
+}
